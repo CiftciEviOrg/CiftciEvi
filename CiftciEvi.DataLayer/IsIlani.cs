0e9ee674b118078eefa94dbd9902c9a968888295
aydın gültepe <aydngltp@gmail.com>
2018-11-24 19:08:46 +0300

    Case 196 İş İlanı Modeli: Oluşturma

---
+++ CiftciEvi.DataLayer/IsIlani.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftciEvi.DataLayer
+{
+    public class IsIlani
+    {
+        public int Id { get; set; }
+        public string CalismaSekli { get; set; }
+        public int Deneyim { get; set; }
+        public Kategori Kategori { get; set; }
+    }
+}
