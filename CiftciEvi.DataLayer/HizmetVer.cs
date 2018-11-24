0cb2c509e7601bafa9f4e312c9e0cf7364843213
aydın gültepe <aydngltp@gmail.com>
2018-11-24 19:07:34 +0300

    Case 167 Hizmet Verme Modeli: Oluşturma

---
+++ CiftciEvi.DataLayer/HizmetVer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftciEvi.DataLayer
+{
+    public class HizmetVer
+    {
+        public int Id { get; set; }
+        public string Tur { get; set; }
+        public int Deneyim { get; set; }
+        public int DegerlendirmePuan { get; set; }
+        public Kategori Kategori { get; set; }
+    }
+}
