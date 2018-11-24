97a244969b26abb5e43034e01ea725e19ceb454a
aydın gültepe <aydngltp@gmail.com>
2018-11-24 19:10:04 +0300

    Case 254 Gündem Modeli: Oluşturma

---
+++ CiftciEvi.DataLayer/Gundem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftciEvi.DataLayer
+{
+    public class Gundem
+    {
+        public int Id { get; set; }
+        public string Baslik { get; set; }
+        public string Yazi { get; set; }
+        public Resim Resim { get; set; }
+        public int DegerlendirmePuan { get; set; } // tinyint olacak...
+
+        public Kategori Kategori { get; set; }
+
+        public Kullanici Kullanici { get; set; }
+    }
+}
