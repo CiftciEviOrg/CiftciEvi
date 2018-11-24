2fb104fe5160bd8076d160d00a96d1af80594be3
aydın gültepe <aydngltp@gmail.com>
2018-11-24 19:02:39 +0300

    Case 101 Kategori Modeli: Oluşturma

---
+++ CiftciEvi.DataLayer/Kategori.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftciEvi.DataLayer
+{
+    public class Kategori
+    {
+        public int Id { get; set; }
+        public string KategoriAdi { get; set; }
+
+        public List<Ilan> Ilan { get; set; }
+
+    }
+}
