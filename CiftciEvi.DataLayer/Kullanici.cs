025bef63576271f054773350ddff7094dab35058
aydın gültepe <aydngltp@gmail.com>
2018-11-24 18:56:55 +0300

    Case 22 Kullanıcı Modeli: Oluşturma

---
+++ CiftciEvi.DataLayer/Kullanici.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftciEvi.DataLayer
+{
+    public class Kullanici
+    {
+        public int Id { get; set; }
+
+        //[Column(TypeName = "varchar(30)")]
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string Email { get; set; }
+        public string Sifre { get; set; }
+        public string SifreConfirm { get; set; }
+        public string Cep { get; set; }
+        public bool Adminmi { get; set; }
+    }
+}
