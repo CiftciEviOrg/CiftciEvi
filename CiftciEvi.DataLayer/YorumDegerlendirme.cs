03dbb84afb42e5cf0e5d6092500b1c2d55862e1b
aydın gültepe <aydngltp@gmail.com>
2018-11-24 19:11:44 +0300

    Case 294 YorumDeğerlendir Modeli: Oluşturma

---
+++ CiftciEvi.DataLayer/YorumDegerlendirme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftciEvi.DataLayer
+{
+    public class YorumDegerlendirme
+    {
+        public int Id { get; set; }
+        public int Puan { get; set; }
+        public string YorumIcerik { get; set; }
+        public int Tur { get; set; }
+        public HizmetVer HizmetVer { get; set; }
+        public Kullanici Kullanici { get; set; }
+        public Gundem Gundem { get; set; }
+    }
+}
