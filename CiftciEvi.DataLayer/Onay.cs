d0de5fe7b2a60a03bd9224a651b2db8b0fcf0581
aydın gültepe <aydngltp@gmail.com>
2018-11-29 01:09:46 +0300

    Extra 4: Onay Modelinin Oluşturulması

---
+++ CiftciEvi.DataLayer/Onay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftciEvi.DataLayer
+{
+    public class Onay
+    {
+        public int Id { get; set; }
+        public bool OnayRed { get; set; }
+        public string Aciklama { get; set; }
+        public Kullanici Kullanici { get; set; }
+
+    }
+}
