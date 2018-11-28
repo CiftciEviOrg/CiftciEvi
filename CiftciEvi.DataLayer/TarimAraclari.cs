d6b02d6297029e93d5f2ad355438c5af51bc07e9
zybk95 <abdulkadirzeybek95@gmail.com>
2018-11-28 21:37:19 +0300

    Case 109 Tarım Araçları Modeli: Oluşturma

---
+++ CiftciEvi.DataLayer/TarimAraclari.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftciEvi.DataLayer
+{
+    public class TarimAraclari
+    {
+        public int Id { get; set; }
+        public string Marka { get; set; }
+        public string Tipi { get; set; }
+        public int Model { get; set; }
+        public sbyte TekerlekSayisi { get; set; }
+        public sbyte Vites { get; set; }
+        public int MotorGucu { get; set; }
+        public int CalismaSaati { get; set; }
+        public string KabinTipi { get; set; }
+        public string CekisTipi { get; set; }
+        public Kategori Kategori { get; set; }
+    }
+}
