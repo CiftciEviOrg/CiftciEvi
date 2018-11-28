61777655195cb2b351130fbf31ff1ba3eecd0edc
zybk95 <abdulkadirzeybek95@gmail.com>
2018-11-28 21:45:22 +0300

    Extra 1 Mesaj Detay Modeli Oluşturma

---
+++ CiftciEvi.DataLayer/MesajDetay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftciEvi.DataLayer
+{
+    class MesajDetay
+    {
+        public int Id { get; set; }
+        public string Icerik { get; set; }
+        public Mesaj MesajId { get; set; }
+    }
+}
