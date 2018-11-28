7bf5e05f990a5676a82021df65a6bc0961428b18
zybk95 <abdulkadirzeybek95@gmail.com>
2018-11-28 21:38:59 +0300

    Case 138 Arazi Modeli: Oluşturma

---
+++ CiftciEvi.DataLayer/Arazi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftciEvi.DataLayer
+{
+    public class Arazi
+    {
+        public int Id { get; set; }
+        public int Mkare { get; set; }
+        public int Su { get; set; }
+        public Agac Agac { get; set; }
+        public Kategori Kategori { get; set; }
+    }
+}
