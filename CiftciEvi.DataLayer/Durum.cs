88e4fc12196853e989a366623d74ee83880fed1f
aydın gültepe <aydngltp@gmail.com>
2018-11-29 01:11:53 +0300

    Extra 7: Durum Modelinin Oluşturulması

---
+++ CiftciEvi.DataLayer/Durum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftciEvi.DataLayer
+{
+    public class Durum
+    {
+        public int Id { get; set; } 
+        public string ZamanDurum { get; set; } //string mi olacak
+        public string KiraDurum { get; set; }
+    }
+}
