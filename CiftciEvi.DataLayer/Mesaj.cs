4a6d2ca0af615f18d13a0498baff67f109a711dc
zybk95 <abdulkadirzeybek95@gmail.com>
2018-11-28 21:43:31 +0300

    Case 317 Mesaj Modeli: Oluşturma

---
+++ CiftciEvi.DataLayer/Mesaj.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftciEvi.DataLayer
+{
+    class Mesaj
+    {
+        public int Id { get; set; }
+        public Kullanici GonderenId { get; set; }
+        public Kullanici AliciId { get; set; }
+        public Ilan Ilan { get; set; }
+    }
+}
