5db0a127e269b78cb48ad41d7df6aeadad414543
zybk95 <abdulkadirzeybek95@gmail.com>
2018-11-28 21:35:06 +0300

    Case 70: Ilan Modeli Olusturuldu

---
+++ CiftciEvi.DataLayer/Ilan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftciEvi.DataLayer
+{
+    public class Ilan
+    {
+        public int Id { get; set; }
+        public string Baslik { get; set; }
+        public string Aciklama { get; set; }
+        public decimal Fiyat { get; set; }
+        public string Kimden { get; set; }
+        public Resim Resim { get; set; }
+        public DateTime Tarih { get; set; }
+        public Durum Durum { get; set; }
+        public Adres Adres { get; set; }
+        public Onay Onay { get; set; }
+
+        public List<Kategori> Kategori { get; set; }
+    }
+}
