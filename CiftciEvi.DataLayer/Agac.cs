46df26faa181737bcf9259d0b85e54f1a7430a40
zybk95 <abdulkadirzeybek95@gmail.com>
2018-11-28 22:05:28 +0300

    Extra 3: Arama Modeli: Oluşturma

---
+++ CiftciEvi.DataLayer/Agac.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftciEvi.DataLayer
+{
+    class Agac
+    {
+        public int Id { get; set; }
+        public string AgacAdi { get; set; }
+        public string Cins { get; set; }
+        public int Adet { get; set; }
+    }
+}
