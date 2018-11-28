2cc77393a96f38cbdebcf1dbfac737bb2b035e1f
zybk95 <abdulkadirzeybek95@gmail.com>
2018-11-28 19:38:49 +0300

    Case 66: Adres Modeli Olusturuldu

---
+++ CiftciEvi.DataLayer/Adres.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftciEvi.DataLayer
+{
+    public class Adres
+    {
+        public int Id { get; set; }
+        public string Ilce { get; set; }
+        public string MahalleKoy { get; set; }
+
+    }
+}
