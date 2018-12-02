449924e4dfd185d5247f752bbb425437b6987abf
aydın gültepe <aydngltp@gmail.com>
2018-12-03 01:39:54 +0300

    Case 168 Hizmet Verme Modeli: Validasyonların Tanımlanması

--- CiftciEvi.DataLayer/HizmetVer.cs
+++ CiftciEvi.DataLayer/HizmetVer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,10 @@ namespace CiftciEvi.DataLayer
     public class HizmetVer
     {
         public int Id { get; set; }
+
+        [Required]
         public string Tur { get; set; }
+        [Required]
         public int Deneyim { get; set; }
         public int DegerlendirmePuan { get; set; }
         public Kategori Kategori { get; set; }
