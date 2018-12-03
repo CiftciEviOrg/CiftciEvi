cbfa0cba5ed99668e3ddb623d839a6a129b25b24
zybk95 <abdulkadirzeybek95@gmail.com>
2018-12-03 03:20:33 +0300

    Case 71: İlan Modeli: Validasyonların Oluşturulması

--- CiftciEvi.DataLayer/Ilan.cs
+++ CiftciEvi.DataLayer/Ilan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,30 @@ namespace CiftciEvi.DataLayer
 {
     public class Ilan
     {
+
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Eposta gereklikdir."),DataType(DataType.Text)]
         public string Baslik { get; set; }
+
+        [Required(ErrorMessage = "Açıklama Alanı Boş Bırakılamaz"), DataType(DataType.Text)]
+        [Display(Name = "Ilan hakkında bilgi veriniz...")]
         public string Aciklama { get; set; }
+
+        [Required(ErrorMessage = "Fiyat gereklidir."), DataType(DataType.Currency)]
         public decimal Fiyat { get; set; }
+
+
         public string Kimden { get; set; }
+
         public Resim Resim { get; set; }
+
         public DateTime Tarih { get; set; }
+
         public Durum Durum { get; set; }
+
         public Adres Adres { get; set; }
+
         public Onay Onay { get; set; }
 
         public List<Kategori> Kategori { get; set; }
