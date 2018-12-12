01887747e1cc95763f00b8c1eed0a231b8c6b121
zybk95 <abdulkadirzeybek95@gmail.com>
2018-12-12 11:35:15 +0300

    DataInitializer Olusturuldu

---
+++ CiftciEvi/Models/DataInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CiftciEvi.Models
+{
+    public class DataInitializer:DropCreateDatabaseIfModelChanges<DataContext>
+    {
+        public override string ToString()
+        {
+            
+
+
+            return base.ToString();
+        }
+    }
+}
