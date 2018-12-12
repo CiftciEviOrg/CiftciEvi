76452199f1a4ab3e35eb3e96fb8f403f8e9b5059
zybk95 <abdulkadirzeybek95@gmail.com>
2018-12-13 00:50:23 +0300

    Case 111: Tarım Aracları Data initializer olusturulması

--- CiftciEvi/Models/TarimAraclari.cs
+++ CiftciEvi/Models/TarimAraclari.cs
@@ -14,14 +14,16 @@ namespace CiftciEvi.Models
         //Extra 8: Marka icin yeni bir model icab etti
         public MarkaTarimArac Marka { get; set; }
 
-        public string Tipi { get; set; }
-        public sbyte ModelYili { get; set; }
+        public sbyte Tipi { get; set; }
+        public int ModelYili { get; set; }
         public sbyte TekerlekSayisi { get; set; }
-        public sbyte Vites { get; set; }
-        public int MotorGucu { get; set; }
+
+        //Extra 10:Vites icin yeni bir model icab etti
+        public VitesTarimArac Vites { get; set; }
+        public int MotorGucuHP { get; set; }
         public int CalismaSaati { get; set; }
-        public string KabinTipi { get; set; }
-        public string CekisTipi { get; set; }
+        public sbyte KabinTipi { get; set; }
+        public sbyte CekisTipi { get; set; }
         public Kategori Kategori { get; set; }
     }
 }
