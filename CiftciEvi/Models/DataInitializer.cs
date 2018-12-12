60814f4d7ca152ed06585f60835951fcd851c82f
zybk95 <abdulkadirzeybek95@gmail.com>
2018-12-13 01:46:56 +0300

    Case 67: Adres il Data initializer olusturulması

--- CiftciEvi/Models/DataInitializer.cs
+++ CiftciEvi/Models/DataInitializer.cs
@@ -152,9 +152,29 @@ namespace CiftciEvi.Models
             #endregion
 
 
+            #region "Il"
+            string[] illerdizi = {"Adana", "Adıyaman", "Afyon", "Ağrı", "Amasya", "Ankara", "Antalya",
+                "Artvin", "Aydın", "Balıkesir", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa",
+                "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Edirne", "Elazığ", "Erzincan",
+                "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Isparta",
+                "İçel(Mersin)", "İstanbul", "İzmir", "Kars", "Kastamonu", "Kayseri", "Kırklareli", "Kırşehir",
+                "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "K.maraş", "Mardin", "Muğla", "Muş",
+                "Nevşehir", "Niğde", "Ordu", "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas",
+                "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Uşak", "Van", "Yozgat",
+                "Zonguldak", "Aksaray", "Bayburt", "Karaman", "Kırıkkale", "Batman", "Şırnak",
+                "Bartın", "Ardahan", "Iğdır", "Yalova", "Karabük", "Kilis", "Osmaniye", "Düzce"};
 
 
+            for (int i = 0; i < illerdizi.Length; i++)
+            {
+                String il = illerdizi[i].ToString();
+                context.iller.Add(new Il
+                {
+                    IlAdi = il
+                });
 
+            }
+            #endregion
 
 
 
