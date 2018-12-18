7c03851a8acc9e2443070c77283d4214b1a17835
zybk95 <abdulkadirzeybek95@gmail.com>
2018-12-18 12:26:52 +0300

    Extra 15: IlanViewModel Eklenmesi

---
+++ CiftciEvi/Models/viewModel/ilanViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CiftciEvi.Models.viewModel
+{
+    public class ilanViewModel
+    {
+        public int UstKategoriId { get; set; }//SelectedCityId        
+        public int AltKAtegoriId { get; set; }//SelectedCountryId
+
+        public SelectList UstKategori { get; set; }
+        public SelectList AltKategori { get; set; }
+    }
+}
